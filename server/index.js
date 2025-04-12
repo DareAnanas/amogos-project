c153c39e3d26a8cd2d912735f2ba8b068b132f46
DareAnanas <nylo2005@gmail.com>
2025-04-12 23:55:26 +0300

    images works but how idk

--- server/index.js
+++ server/index.js
@@ -9,8 +9,8 @@ app.use(cors());
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.join(__dirname, '../dist')));
 app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../dist')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../dist', 'index.html'));
