e8a5263458508e68449a4e0d8681f6017ade8bde
DareAnanas <nylo2005@gmail.com>
2025-04-12 18:23:10 +0300

    made Home global

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,32 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "./components/HomePage";
 import AnimalForm from "./components/AnimalForm";
 import ShelterSearch from "./components/ShelterSearch";
 import ShelterForm from "./components/ShelterForm";
 import VolonteerForm from "./components/VolonteerForm";
 import AnimalList from "./components/AnimalList";
-import LoginPage from "./components/LoginPage";
+import LoginForm from "./components/LoginForm";
+import Home from "./components/Home";
+import Description from "./components/Description";
 import "./App.css";
 
 function App() {
   return (
+    
     <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/search-animals" element={<AnimalList />} />
-        <Route path="/register-shelter" element={<ShelterForm />} />
-        <Route path="/register-volonteer" element={<VolonteerForm />} />
-        <Route path="/post-animal" element={<AnimalForm />} />
-        <Route path="/search-shelters" element={<ShelterSearch />} />
-        <Route path="/user-login" element={<LoginPage />} />
-      </Routes>
+      <Home>
+        <Routes>
+          <Route path="/" element={<Description />} />
+          <Route path="/search-animals" element={<AnimalList />} />
+          <Route path="/register-shelter" element={<ShelterForm />} />
+          <Route path="/register-volonteer" element={<VolonteerForm />} />
+          <Route path="/post-animal" element={<AnimalForm />} />
+          <Route path="/search-shelters" element={<ShelterSearch />} />
+          <Route path="/user-login" element={<LoginForm />} />
+        </Routes>
+      </Home>
     </Router>
+    
   );
 }
 
