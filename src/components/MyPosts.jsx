f3699ae6b96bf7abe685a6518c3d444176c65403
Liqiq <illa77mucolajovuch@gmail.com>
2025-04-13 23:37:35 +0300

    add button added

--- src/components/MyPosts.jsx
+++ src/components/MyPosts.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import globalInstance from "../service/Interceptor";
 import Hamster from "./HamsterLoading";
+import { Link } from "react-router-dom";
 
 function MyPosts() {
   const [animals, setAnimals] = useState([]);
@@ -185,6 +186,15 @@ function MyPosts() {
           <Hamster className="normal"></Hamster>
         </div>
       )}
+      <Link to="/post-animal">
+      <div tabindex="0" class="plusButton">
+    <svg class="plusIcon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
+     <g mask="url(#mask0_21_345)">
+       <path d="M13.75 23.75V16.25H6.25V13.75H13.75V6.25H16.25V13.75H23.75V16.25H16.25V23.75H13.75Z"></path>
+     </g>
+   </svg>
+  </div></Link>
+  
     </div>
   );
 }
