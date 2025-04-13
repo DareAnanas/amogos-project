9c614d7777c44b48308c6b701359b65cf2b201f0
Liqiq <illa77mucolajovuch@gmail.com>
2025-04-13 14:11:59 +0300

    ShelterForm is connected too and interceptor now goes to / when 401

--- src/service/Interceptor.js
+++ src/service/Interceptor.js
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
 const globalInstance = axios.create({
-  baseURL: 'https://bd-h8ye.onrender.com/',
+  baseURL: "https://bd-h8ye.onrender.com/",
   timeout: 5000,
 });
 
@@ -17,9 +17,14 @@ globalInstance.interceptors.request.use(
 
 globalInstance.interceptors.response.use(
   (response) => {
+    // Axios already parses JSON responses; just return the response.
     return response;
   },
   (error) => {
+    if (error.response && error.response.status === 401) {
+      // If 401 Unauthorized, redirect to home page
+      window.location.href = "/";
+    }
     return Promise.reject(error);
   }
 );
